5688be0be969c1199cc1a370de57f04a27329edf
sfntlls <91127808+sfntlls@users.noreply.github.com>
2023-08-02 18:25:25 +0800

    Update peo.js

--- peo.js
+++ peo.js
@@ -1,35 +1,3 @@
-window.addEventListener('DOMContentLoaded', () => {
-    const navContainer = document.querySelector('.nav-container');
-    const scrollUpButton = document.getElementById('scrollUpButton');
-    const scrollDownButton = document.getElementById('scrollDownButton');
-
-    navContainer.addEventListener('scroll', handleScroll);
-
-    function handleScroll() {
-        if (navContainer.scrollTop > 0) {
-            scrollUpButton.classList.add('show');
-        } else {
-            scrollUpButton.classList.remove('show');
-        }
-
-        if (navContainer.scrollTop + navContainer.clientHeight < navContainer.scrollHeight) {
-            scrollDownButton.classList.add('show');
-        } else {
-            scrollDownButton.classList.remove('show');
-        }
-    }
-});
-	
-function scrollNavContainer(direction) {
-    const navContainer = document.querySelector('.nav-container');
-    const scrollAmount = 150; 
-    if (direction === 'up') {
-        navContainer.scrollTop -= scrollAmount;
-    } else if (direction === 'down') {
-        navContainer.scrollTop += scrollAmount;
-    }
-}
-
 function toggleDetails(id) {
     const peoContainer = document.getElementById(id);
     const details = peoContainer.querySelector('p');
